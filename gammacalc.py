ddaad58927c3b42ac3bfc7f4e8581e675fd8abc9
ayoola-babatunde <ayoola@minerva.kgi.edu>
2021-09-22 11:29:18 -0700

    Added scipy optimize

--- gammacalc.py
+++ gammacalc.py
@@ -3,12 +3,17 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as ptch
 import numpy as np
 import math
+from scipy.spatial.distance import euclidean
+from scipy.optimize import minimize
 #%%
 #data
-x = [1, 4, 3]
-y = [1, 1, 2]
+p_ = (1, 1)
+r_ = (4, 1)
+o_ = (3, 0)
+all_points = [p_,r_,o_]
 labels = ['P', 'R', 'O']
 
+#%%
 #https://stackoverflow.com/questions/55816902/finding-the-intersection-of-two-circles
 def get_intersections(x0, y0, r0, x1, y1, r1):
     # circle 1: (x0, y0), radius r0
@@ -39,49 +44,72 @@ def get_intersections(x0, y0, r0, x1, y1, r1):
         return (x3, y3, x4, y4)
 
 
+def obj_func(gamma, p, r, o): 
+    t = 0.25
+    c = 4 
+    d = euclidean(p,r)
+
+    R_p = gamma*d*t
+    R_r = gamma*d*(1-t)
+    R_a = c*d/gamma
+    #print(gamma)
+    inters = get_intersections(p[0],p[1],(R_a-R_p), r[0],r[1],(R_a-R_r))
+    C_a1x, C_a1y, C_a2x, C_a2y = inters
+    #print(inters)
+    dist_Ca1_o = euclidean((C_a1x, C_a1y), o)
+    dist_Ca2_o = euclidean((C_a2x, C_a2y), o)
+    return abs(max(dist_Ca1_o, dist_Ca2_o) - R_a)
+    
+minimize(obj_func, args=(p_,r_,o_), x0 = 0.5)
+
 #%%
 
-#plt.ylim(0, 4); plt.xlim(0,4)
-#circ1 = plt.Circle((1,1),1, fill = False)
-#ax.add_patch(circ1)
-
-
-
-for i, val in enumerate(labels):
-    ax.annotate(val, (x[i], y[i]))
-
-
+#testing gamma
 t = 0.25 #weird scaling factor
-d = 3 #euclidean distance
+d = euclidean(p,r) #euclidean distance
 c = 4 #weird scaling factor
 
-for gamma in np.arange(0.0001, 0.5, 0.025): 
+R_a_store = []
+plot = True
+
+for gamma in np.arange(0.0001, 0.7, 0.05): 
     print(gamma)
 
     R_p = gamma*d*t
     R_r = gamma*d*(1-t)
     R_a = c*d/gamma
+    R_a_store.append(R_a)
 
-    inters = get_intersections(1,1,(R_a-R_p), 4,1,(R_a-R_r))
-    print(inters)
+    C_a1x, C_a1y, C_a2x, C_a2y = get_intersections(p[0],p[1],(R_a-R_p), r[0],r[1],(R_a-R_r))
+    #print(inters)
+    dist_Ca1_o = euclidean((C_a1x, C_a1y), o)
+    dist_Ca2_o = euclidean((C_a2x, C_a2y), o)
 
-    fig, ax = plt.subplots()
-    ax.set_aspect('equal')
+    print(abs(max(dist_Ca1_o, dist_Ca2_o) - R_a)) 
 
-    plt.scatter(x,y)
-    C_p = plt.Circle((x[0], y[0]), R_p, fill = False)
-    C_r = plt.Circle((x[1], y[1]), R_r, fill = False)
-    C_a1 = plt.Circle((inters[0], inters[1]), R_a, fill = False, color = 'red')
-    C_a2 = plt.Circle((inters[2], inters[3]), R_a, fill = False, color = 'red')
-    #A_1 = ptch.Arc((inters[0], inters[1]), R_a*2, R_a*2, 0, 90, 115)
-    ax.add_patch(C_p)
-    ax.add_patch(C_r)
-    ax.add_patch(C_a1)
-    ax.add_patch(C_a2)
+    if plot: 
+        fig, ax = plt.subplots()
+        ax.set_aspect('equal')
+        x = [x[0] for x in all_points]
+        y = [y[1] for y in all_points]
 
-    plt.ylim(-2,5)
-    plt.xlim(-2,10)
-    plt.show()
+        ax.scatter(x,y)
+        C_p = plt.Circle((x[0], y[0]), R_p, fill = False)
+        C_r = plt.Circle((x[1], y[1]), R_r, fill = False)
+        C_a1 = plt.Circle((C_a1x, C_a1y), R_a, fill = False, color = 'red')
+        C_a2 = plt.Circle((C_a2x, C_a2y), R_a, fill = False, color = 'orange')
+        print(dist_Ca1_o, dist_Ca2_o, R_a)
+        #A_1 = ptch.Arc((C_a1x, C_a1y), R_a*2, R_a*2, 0, 90, 115)
+        ax.add_patch(C_p)
+        ax.add_patch(C_r)
+        ax.add_patch(C_a1)
+        ax.add_patch(C_a2)
+        ax.plot([o[0], C_a1x], [o[1], C_a1y], color = 'red')
+        ax.plot([o[0], C_a2x], [o[1], C_a2y], color = 'orange')
+
+        plt.ylim(-2,5)
+        plt.xlim(-2,6)
+        plt.show()
 
 #%%
 #Arc?
@@ -113,4 +141,5 @@ for x in range(5000):
 
 plt.scatter(stor_x, stor_y, marker='.')
 plt.xlim(left=0)
+plt.vlines(30, -2000,2000)
 # %%
